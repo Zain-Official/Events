e05476e5e28dcb48c83a15b42c1f4699b29db4bf
Zain <zainofficial03@gmail.com>
2021-05-14 01:11:49 +0500

    Add project files.

---
+++ Events/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Events
+{
+    public partial class Form1 : Form
+    {
+        public int a = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new System.EventHandler(this.button1_Click);
+            button1.Click += new System.EventHandler(this.button1_Click);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(a.ToString());
+            a++;
+
+        }
+    }
+}
